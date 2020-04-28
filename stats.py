c7d07f40979afd0676d2cc137a8475beb4214558
TirianShirley <tcshirley@ucdavis.edu>
2020-04-27 17:38:09 -0700

    finished assignment 4 right this time

--- stats.py
+++ stats.py
@@ -6,7 +6,7 @@ import fileinput
 # Write a program that computes typical stats
 # Count, Min, Max, Mean, Std. Dev, Median
 # No, you cannot import any other modules!
-count = i
+
 data = []
 for line in fileinput.input():
 	if line.startswith('#'): continue
@@ -15,15 +15,28 @@ for line in fileinput.input():
 data.sort()
 min = data[0]
 max = data[-1]
-
-
-
-print(f'Count: {count}')
+sum = 0
+for i in range(len(data)):
+	sum += data[i]
+mean = sum/len(data)
+ssum = 0
+for x in data:
+	ssum += (mean - x)**2
+std = sqrt(ssum/len(data))
+median = None
+if len(data) % 2 == 1:
+	mi = int(len(data)/2)
+	median = data[mi]
+else:
+	hi = int(len(data)/2)
+	lo = hi -1
+	median = (data[lo] + data[hi])/2
+print(f'Count: {len(data)} ')
 print(f'Minimum: {min}')
 print(f'Maximum: {max}')
-print(f'Mean: ')
-print(f'Std. dev: ')
-print(f'Median: ')
+print(f'Mean: {sum/len(data)} ')
+print(f'Std. dev: {std:.3f} ')
+print(f'Median: {median}')
 """
 python3 stats.py numbers.txt
 Count: 10
